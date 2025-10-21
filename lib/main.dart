e4bb697281e1d43f550cef3557718e4801d5fed2
Muhammad Ali <malitariq5324@gmail.com>
2025-10-21 18:03:21 +0500

    Integrate Hive for local data persistence
    - Initialize Hive for Flutter (`Hive.initFlutter()`) in `main()`
    - Register `TaskAdapter` for `Task` model serialization
    - Open a Hive box named `tasksBox` to store tasks
    - Ensure Flutter bindings are initialized before async operations in `main`

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:todey/screens/tasks_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'utilities/items_class.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
+  await Hive.openBox<Task>('tasksBox');
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});

1d80ef269b8d51151cef15da8824f9e50faeefb3
Muhammad Ali <malitariq5324@gmail.com>
2025-10-25 20:52:18 +0500

    Create HiveHelper class for task data management
    - Introduced `HiveHelper` to abstract Hive database operations for tasks.
    - Implemented `addTask`, `updateItemState`, and `deleteItem` methods to manage the task list.
    - Added `initializeItems` to populate the database with default tasks on the first run.
    - Managed a `TextEditingController` for the task input field.
    - Included methods to update the local `items` list from the Hive box.
    - Provided utility methods to close the Hive box and dispose of the controller.

---
+++ lib/utilities/HiveHelperClass.dart
@@ -0,0 +1,59 @@
+import 'package:hive/hive.dart';
+import '../utilities/items_class.dart';
+import 'package:flutter/material.dart';
+
+class HiveHelper {
+  final Box<Task> _tasksBox = Hive.box<Task>('tasksBox');
+  late List<Task> items;
+  final TextEditingController inputFieldController = TextEditingController();
+  HiveHelper() {
+    initializeItems();
+  }
+
+  void addTask(Task newTask) {
+    inputFieldController.clear();
+    _tasksBox.add(newTask);
+    updateItemsList();
+  }
+
+  void initializeItems() {
+    if (_tasksBox.isEmpty) {
+      items = [
+        Task(title: 'Take Flutter Lecture', checkedState: false),
+        Task(title: 'Submit a Task on Leetcode', checkedState: true),
+        Task(title: 'Submit a Task on Geeksforgeeks', checkedState: false),
+      ];
+      updateHiveBox();
+    } else {
+      updateItemsList();
+    }
+  }
+
+  void updateItemsList({bool mounted = true}) {
+    if (mounted) {
+      items = _tasksBox.values.toList();
+    }
+  }
+
+  Future updateItemState(int index, bool? newValue) async {
+    final task = _tasksBox.getAt(index);
+    task?.checkedState = newValue ?? task.checkedState;
+    await task?.save();
+    updateItemsList();
+  }
+
+  Future deleteItem(int index) async {
+    await _tasksBox.deleteAt(index);
+    updateItemsList();
+  }
+
+  void updateHiveBox() {
+    for (var item in items) {
+      _tasksBox.add(item);
+    }
+  }
+
+  void clearInputField() => inputFieldController.clear();
+  void disposeInputTextController() => inputFieldController.dispose();
+  void closeHiveBox() => _tasksBox.close();
+}
